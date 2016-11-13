f08f80787da319ca7a3e6132b6060a56210a6927
Rex Low <qiweilow950823@gmail.com>
2016-11-13 14:25:16 +0800

    First commit

---
+++ Greetings.java
@@ -0,0 +1,7 @@
+public class Greetings {
+  public static void main(String args[]) {
+    String title = "Green house";
+    int length = title.length();
+    System.out.println(title + length);
+  }
+}
---
+++ Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+  public static void main(String args[]) {
+    Scanner scanner = new Scanner(System.in);
+    int inputInt = scanner.nextInt();
+    String inputString = scanner.next();
+    scanner.close();
+
+    System.out.println(inputString + " " + inputInt);
+  }
+}
