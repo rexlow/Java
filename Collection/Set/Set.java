f9d47b66b36dde47e06a5c7462cdfe9e91e67b54
Rex Low <qiweilow950823@gmail.com>
2017-01-07 01:09:44 +0800

    Set

---
+++ Collection/Set/Set.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class Set {
+  public static void main(String[] args) {
+
+    HashSet<String> fruit = new HashSet<String>();
+
+    fruit.add("rambutan");
+    fruit.add("apple");
+    fruit.add("orange");
+
+    System.out.println(fruit);
+    
+  }
+}
